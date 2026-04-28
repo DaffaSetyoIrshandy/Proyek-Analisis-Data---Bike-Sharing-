8e93f218bcadb4b5e61230fa725e81c06adadb40
Daffa Setyo <164187061+DaffaSetyoIrshandy@users.noreply.github.com>
2026-04-28 14:15:20 +0700

    Implement data analysis and visualization for bike sharing
    Added data loading and visualization for daily and hourly bike rentals.

--- app.py
+++ app.py
@@ -11,12 +11,18 @@ def df_harian_count(df_harian):
     df_harian_2011 = df_harian.query(str('dteday >= "2011-01-01" and dteday < "2012-12-31"'))
     return df_harian_count_2011
 
+Harian_df = pd.read_csv("dashboard/df_harian_clean.csv")
+Perjam_df = pd.read_csv("dashboard/df_Perjam_clean.csv")
 
 Harian_df['dteday'] = pd.to_datetime(Harian_df['date'])
 Perjam_df['dteday'] = pd.to_datetime(Perjam_df['date'])
 
 st.set_page_config(layout="wide")
 st.title("Analisis Data Bike Sharing")
+
+daily_count = Harian_df.groupby('dteday').agg({'casual': 'sum', 'registered': 'sum'}).reset_index()
+daily_count.rename(columns={'casual': 'casual_user', 'registered': 'registered_user'}, inplace=True)  
+
 
 fig, ax = plt.subplots(figsize=(12, 6))
 sns.lineplot(x='dteday', y='casual_user', data=daily_count, label='Casual Users', ax=ax)
@@ -33,8 +39,20 @@ st.subheader(" Data Penyewaan Berdasarkan Jam ")
 hourly_count = Perjam_df.groupby('hour')['count'].sum()
 fig, ax = plt.subplots(figsize=(10, 6))
 hourly_count.plot(kind='bar', ax=ax)
+ax.set_title('Jumlah Penyewaan Berdasarkan Jam')
+ax.set_xlabel('Jam')
+ax.set_ylabel('Jumlah Penyewaan')
+plt.tight_layout()
+st.pyplot(fig)
+
+
 
 st.subheader(" Data Penyewaan Berdasarkan Season")
 season_count = Harian_df.groupby('season')['count'].sum()
 fig, ax = plt.subplots(figsize=(10, 6))
 season_count.plot(kind='bar', ax=ax)
+ax.set_title('Jumlah Penyewaan Berdasarkan Musim')
+ax.set_xlabel('Musim')
+ax.set_ylabel('Jumlah Penyewaan')
+plt.tight_layout()
+st.pyplot(fig)
