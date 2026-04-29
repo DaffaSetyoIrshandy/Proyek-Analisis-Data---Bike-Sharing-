2847b11ef63c1dcec951c2a05a711c74a4f38605
Daffa Setyo <164187061+DaffaSetyoIrshandy@users.noreply.github.com>
2026-04-30 01:04:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,13 +20,15 @@ start_date, end_date = st.sidebar.slider(
     "Pilih Rentang Tanggal Berdasarkan Harian",
     min_value=Harian_df['date'].min(),
     max_value=Harian_df['date'].max()
-    value=(Harian_df['date'].min()),Harian_df['date'].max())
+    value=(Harian_df['date'].min(), Harian_df['date'].max())
+)
 
 start_date, end_date = st.sidebar.slider(
     "Pilih Rentang Tanggal Berdasarkan Perjam",
     min_value=Perjam_df['date'].min(),
     max_value=Perjam_df['date'].max()
-    value=(Perjam_df['date'].min(),Perjam_df['date'].max())
+    value=(Perjam_df['date'].min(), Perjam_df['date'].max())
+)
 
 # Season filter
 selected_season = st.sidebar.multiselect(
