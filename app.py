0321855679b64f39fdc87bf4495cab136d65b768
Daffa Setyo <164187061+DaffaSetyoIrshandy@users.noreply.github.com>
2026-04-30 03:53:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,14 +46,13 @@ start_date, end_date = st.sidebar.slider(
 )
 
 # Season filter
-Harian_df['season_name'] = Harian_df['season'].map(season_map)
 season_names_list = ["Spring", "Summer", "Fall", "Winter"]
 selected_season = st.sidebar.multiselect(
     "Pilih Musim",
     options=season_names_list,
     default=season_names_list
 )
-
+Harian_df['season_name'] = Harian_df['season'].map(season_map)
 # Hour filter
 hours_str = [f"{i:02d}:00" for i in range(24)]
 
