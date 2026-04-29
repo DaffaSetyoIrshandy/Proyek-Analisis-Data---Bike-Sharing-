59d4fbc421bfc22047d3ed18bd5ec37301c7fb30
Daffa Setyo <164187061+DaffaSetyoIrshandy@users.noreply.github.com>
2026-04-30 04:27:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ start_date, end_date = st.sidebar.slider(
 
 # Season filter
 Harian_df['season_name'] = Harian_df['season'].map(season_map)
-season_name = ["Spring", "Summer", "Fall", "Winter"]
-selected_season_name = st.sidebar.multiselect(
+season_names = ["Spring", "Summer", "Fall", "Winter"]
+selected_season_names = st.sidebar.multiselect(
     "Pilih Musim",
-    options=season_name_list,
-    default=season_name_list
+    options=season_names,
+    default=season_names
 )
 
 # Hour filter
@@ -85,7 +85,7 @@ selected_weekdays = [k for k, v in weekday_names_map.items() if v in selected_we
 filtered_harian = Harian_df[
     (Harian_df['dteday'] >= pd.to_datetime(start_date)) &
     (Harian_df['dteday'] <= pd.to_datetime(end_date)) &
-    (Harian_df['season_name'].isin(selected_season_name)) &
+    (Harian_df['season_names'].isin(selected_season_names)) &
     (Harian_df['month_name'].isin(selected_month_names)) &
     (Harian_df['weekday_name'].isin(selected_weekday_names))
 ]
